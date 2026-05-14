3c2472521f6a6908d30767ae60d95816481567b0
BradenLemna <lemna.business@gmail.com>
2026-05-14 12:20:58 -0500

    Update CORS policy
    Allows traffic from the myscene domain as well as a local domain for testing

--- src/backend/serversideapi/apiServer/index.js
+++ src/backend/serversideapi/apiServer/index.js
@@ -6,8 +6,21 @@
 import { autoCompleteCity, getLatitude, getLongitude, getLocation } from "../apiCalls/locationIQAPICall.js";
 import { getGenre } from "../apiCalls/lastFMAPICall.js";
 import express from 'express';
+import cors from 'cors';
+
+const allowedOrigins = ['https://myscene.live', 'http://127.0.0.1:5500'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
 
 const app = express();
+app.use(cors(corsOptions));
 const port = 3000;
 
 app.listen(port, () => {
