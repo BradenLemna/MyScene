e4b50e6f80a391e396037ed714c44fc5a0a43d8d
BradenLemna <lemna.business@gmail.com>
2026-05-15 21:41:53 -0500

    Create Update Scripts
    Added update scripts for automatically updating the api, website, and db files.

---
+++ updateScripts/apiUpdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script to automate updating API items form github repository
+
+echo "Running update script"
+
+rm -rf ~/myscene-git
+rm -rf ~/myscene-api/apiServer/*
+rm -rf ~/myscene-api/apiCalls/*
+
+git clone https://github.com/BradenLemna/MyScene.git ~/myscene-git
+
+cp -r ~/myscene-git/src/backend/serversideapi/apiServer/* ~/myscene-api/apiServer/
+cp -r ~/myscene-git/src/backend/serversideapi/apiCalls/* ~/myscene-api/apiCalls/
+
+pm2 reload index
---
+++ updateScripts/webUpdate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script to automate upadating webserver items form github repository
+
+echo "Running update script"
+
+rm -rf ~/myscene-git
+git clone https://github.com/BradenLemna/MyScene.git ~/myscene-git
+
+if [ "$1" == "FullUpdate" ]; then
+    echo "Running Full Database Update (Removing All Database Files)"
+    rm -rf ~/myscene-db
+elif [ "$1" == "" ]; then
+    read -p "Would you like to run a full database update? (Y or N): " input
+fi
+
+echo "Updating Website"
+
+cp -r ~/myscene-git/. ~/var/www/html
+
+if [ "$1" == "FullUpdate" ]; then
+    echo "Updating sql File"
+    cp -r ~/myscene-git/schema.sql ~/myscene-db
+elif [ "$input" == "Y" ] || [ "$input" == "y" ]; then
+    echo "Running Full Database Update (Removing All Database Files)"
+    rm -rf ~/myscene-db
+    echo "Updating sql File"
+    cp -r ~/myscene-git/schema.sql ~/myscene-db
+fi
+
+cp -r ~/myscene-git/src/backend/db-api/. ~/myscene-db/db-api
