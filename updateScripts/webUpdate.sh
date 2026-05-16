92372b402b92b11a281236008a3cdd53a13859ea
BradenLemna <lemna.business@gmail.com>
2026-05-16 03:47:32 -0500

    Update webUpdate.sh
    Fixed pathing

--- updateScripts/webUpdate.sh
+++ updateScripts/webUpdate.sh
@@ -9,7 +9,7 @@ git clone https://github.com/BradenLemna/MyScene.git ~/myscene-git
 
 if [ "$1" == "FullUpdate" ]; then
     echo "Running Full Database Update (Removing All Database Files)"
-    rm -rf ~/myscene-db
+    rm -rf ~/myscene-db/*
 elif [ "$1" == "" ]; then
     read -p "Would you like to run a full database update? (Y or N): " input
 fi
@@ -23,7 +23,7 @@ if [ "$1" == "FullUpdate" ]; then
     cp -r ~/myscene-git/schema.sql ~/myscene-db/schema.sql
 elif [ "$input" == "Y" ] || [ "$input" == "y" ]; then
     echo "Running Full Database Update (Removing All Database Files)"
-    rm -rf ~/myscene-db
+    rm -rf ~/myscene-db/*
     echo "Updating sql File"
     cp -r ~/myscene-git/schema.sql ~/myscene-db/schema.sql
 fi
