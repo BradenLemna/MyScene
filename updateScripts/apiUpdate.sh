c85e880a1a613ca53dfb58e8dfcac15729e7e252
BradenLemna <lemna.business@gmail.com>
2026-07-28 15:48:09 -0500

    Update apiUpdate.sh
    Change file locations

--- updateScripts/apiUpdate.sh
+++ updateScripts/apiUpdate.sh
@@ -6,20 +6,20 @@ echo "Running update script"
 
 rm -rf ~/myscene-git
 mkdir ~/myscene-git
-rm -rf ~/myscene-api/apiServer/*
-rm -rf ~/myscene-api/apiCalls/*
+rm -rf /var/www/myscene-api/apiServer/*
+rm -rf /var/www/myscene-api/apiCalls/*
 rm -rf /var/www/myscene-api/*.php
 
 git clone https://github.com/BradenLemna/MyScene.git ~/myscene-git
 
-cp -r ~/myscene-git/src/backend/serversideapi/apiServer/* ~/myscene-api/apiServer/
-cp -r ~/myscene-git/src/backend/serversideapi/apiCalls/* ~/myscene-api/apiCalls/
-cp -r ~/myscene-git/src/backend/db-api/* ~/var/www/myscene-api/
-cp ~/myscene-git/src/backend/db.php ~/var/www/myscene-api/db.php
+cp -r ~/myscene-git/src/backend/serversideapi/apiServer/* /var/www/myscene-api/apiServer/
+cp -r ~/myscene-git/src/backend/serversideapi/apiCalls/* /var/www/myscene-api/apiCalls/
+cp -r ~/myscene-git/src/backend/db-api/* /var/www/myscene-api/db-api/
+cp ~/myscene-git/src/backend/db.php /var/www/myscene-api/db-api/db.php
 
-chmod +x ~/var/www/myscene-api/*.php
+chmod +x /var/www/myscene-api/*.php
 
 cp ~/myscene-git/updateScripts/apiUpdate.sh ~/apiUpdate.sh
 chmod +x ~/apiUpdate.sh
 
-pm2 reload ~/myscene-api/apiServer/index.js
+pm2 reload /var/www/myscene-api/apiServer/index.js
