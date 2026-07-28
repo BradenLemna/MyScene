62b11ee460b08c80f1210db38e7609506c024053
BradenLemna <lemna.business@gmail.com>
2026-07-27 23:17:17 -0500

    Fix url handling

--- src/backend/serversideapi/apiServer/index.js
+++ src/backend/serversideapi/apiServer/index.js
@@ -139,7 +139,7 @@ app.post('/getFeaturedArtists', async (req, res) => {
 });
 
 app.post('/getSimilarArtists', async (req, res) => {
-    const { music_genre } = req.body;
+    const { music_genre } = req.query.genre;
     fetch("/../db-api/search_genre.php", {
         method: "POST",
         headers: {
